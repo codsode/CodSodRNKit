6c29b916592827b9a4cbcd4e942a702be6eddaba
Vishal Chaturvedi <vishalchaubey0011@gmail.com>
2025-01-05 01:22:16 +0530

    Some Changes

--- post-init.script.js
+++ post-init.script.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-const { green, blue, white, gray } = require("kleur");
+const { execSync } = require("child_process");
+const { green, blue, white, gray, red } = require("kleur");
 
+// Function to print ASCII art centered
 function printCenteredAsciiArt(asciiArt) {
   const terminalWidth = process.stdout.columns;
   const lines = asciiArt.split("\n");
@@ -37,7 +39,22 @@ const asciiArt = `
 
 printCenteredAsciiArt(asciiArt);
 
-processComplete();
+function installDependencies() {
+  try {
+    console.log(
+      blue("\n⠹ Installing dependencies with '--legacy-peer-deps'...\n")
+    );
+    execSync("npm install --legacy-peer-deps", { stdio: "inherit" });
+    console.log(green("\n🎉 Dependencies installed successfully! 🎉\n"));
+  } catch (error) {
+    console.error(
+      red(
+        "\n❌ Failed to install dependencies. Please run 'npm install --legacy-peer-deps' manually.\n"
+      )
+    );
+    process.exit(1);
+  }
+}
 
 function processComplete() {
   console.log("\n");
@@ -47,10 +64,13 @@ function processComplete() {
     green("👏 Congratulations! Your project is now set up and ready to go.")
   );
   console.log("\n");
-
   console.log(
     green("🎉 Thank you for choosing our boilerplate. Happy coding! 🎉")
   );
   console.log("\n");
-  process.exit(0);
+
+  installDependencies();
 }
+
+// Start the process
+processComplete();
